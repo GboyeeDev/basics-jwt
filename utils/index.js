2bd507b0fecf03a7bb64a20f1136352525174c9e
Olugboyega <114001863+GboyeeDev@users.noreply.github.com>
2023-04-05 19:25:46 +0100

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,6 @@ const Joi = require('joi')
 const registerSchema = Joi.object().keys({ 
     username: Joi.string().required(),
     password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
- })
+ });
 
  module.exports = registerSchema
